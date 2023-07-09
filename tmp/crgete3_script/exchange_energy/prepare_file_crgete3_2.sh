4261dfb2d5a72759d60bc5a9c3e9a2e51baa984f
weiluntang89 <89478805+weiluntang89@users.noreply.github.com>
2023-07-08 20:48:19 -0700

    Add files via upload

---
+++ tmp/crgete3_script/exchange_energy/prepare_file_crgete3_2.sh
@@ -0,0 +1,5 @@
+cp /home/weilunt/test_script/test/script_cleanup/crgete_test/INCAR .
+cp /home/weilunt/test_script/test/script_cleanup/crgete_test/KPOINTS .
+cp /home/weilunt/test_script/test/script_cleanup/crgete_test/slurm.vasp .
+cp /home/weilunt/test_script/test/script_cleanup/crgete_test/energy.sh .
+cp /home/weilunt/test_script/test/script_cleanup/crgete_test/exchange_energy.py .
