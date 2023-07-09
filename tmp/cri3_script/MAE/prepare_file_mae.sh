65135bf82cf69dd15664aaec535e8c849a51e7cc
weiluntang89 <89478805+weiluntang89@users.noreply.github.com>
2023-07-08 20:40:29 -0700

    Add files via upload

---
+++ tmp/cri3_script/MAE/prepare_file_mae.sh
@@ -0,0 +1,5 @@
+cp /home/weilunt/test_script/test/script_cleanup/test/mae/INCAR .
+cp /home/weilunt/test_script/test/script_cleanup/test/mae/KPOINTS .
+cp /home/weilunt/test_script/test/script_cleanup/test/mae/slurm.vasp .
+cp /home/weilunt/test_script/test/script_cleanup/test/mae/mae_ncl.sh .
+cp /home/weilunt/test_script/test/script_cleanup/test/mae/mae.py .
