46c3bc2042e20571fe3273ba0dc6a160448bc8b3
weiluntang89 <89478805+weiluntang89@users.noreply.github.com>
2023-07-08 20:34:53 -0700

    Add files via upload

---
+++ tmp/cri3_script/prepare_file_crci3_2.sh
@@ -0,0 +1,5 @@
+cp /home/weilunt/test_script/test/script_cleanup/INCAR .
+cp /home/weilunt/test_script/test/script_cleanup/KPOINTS .
+cp /home/weilunt/test_script/test/script_cleanup/slurm.vasp .
+cp /home/weilunt/test_script/test/script_cleanup/energy.sh .
+cp /home/weilunt/test_script/test/script_cleanup/exchange_energy.py .
